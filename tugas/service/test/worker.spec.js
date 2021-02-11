627cef56df0d8ff026c5046563835a26194cf38b
Rahmatullah Arrizal <rahmatullaharrizal@gmail.com>
2021-02-11 11:11:58 +0700

    tugas

---
+++ tugas/service/test/worker.spec.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+
+describe('worker', () => {
+  describe('worker', () => {
+    it('get test', async () => {
+      const test = 'test';
+      expect(test).toBe('test');
+    });
+  });
+});
