3f45fb4b9022ce81ff85fc317c85ef7b35e01c4d
Annas Adharuqudni <57264462+adharuqudni@users.noreply.github.com>
2021-02-11 14:36:31 +0700

    Test CI

--- tugas/service/test/worker.spec.js
+++ tugas/service/test/worker.spec.js
@@ -1,10 +1,5 @@
 /* eslint-disable no-undef */
 
 describe('worker', () => {
-  describe('worker', () => {
-    it('get test', async () => {
-      const test = 'test';
-      expect(test).toBe('test');
-    });
-  });
+  it('worker', () => {});
 });
