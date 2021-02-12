8126167585c630c2d3bde17c208ade821d37b689
Erlangga15 <55235710+Erlangga15@users.noreply.github.com>
2021-02-12 11:19:14 +0700

    update env mysql

--- tugas/service/test/worker.spec.js
+++ tugas/service/test/worker.spec.js
@@ -58,7 +58,7 @@ describe('Worker Service', () => {
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: '',
+        password: 'password',
         database: 'dubnium',
       });
     } catch (err) {
