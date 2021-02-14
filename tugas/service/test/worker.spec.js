d9d3df8a956064fb9542d6600abc80ae2b84743e
Praga Hartyanto Prabowo <hartyanto@gmail.com>
2021-02-15 06:27:25 +0700

    fix lint

--- tugas/service/test/worker.spec.js
+++ tugas/service/test/worker.spec.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 
 const { connect } = require('../lib/orm');
-const { WorkerSchema } = require('../worker/worker.model')
-const { config } = require('../config')
-const server  = require('../worker/server')
+const { WorkerSchema } = require('../worker/worker.model');
+const { config } = require('../config');
+const server = require('../worker/server');
 const http = require('http');
 
 describe('worker', () => {
@@ -18,8 +18,8 @@ describe('worker', () => {
 describe('worker', () => {
   let connection;
   beforeAll(async () => {
-    connection = await connect([ WorkerSchema ], config.database)
-    server.run()
+    connection = await connect([WorkerSchema], config.database);
+    server.run();
   });
   afterAll(async () => {
     await connection.close();
@@ -28,9 +28,8 @@ describe('worker', () => {
 
   describe('info', () => {
     it('get info', async () => {
-     
       function getInfo() {
-        return new Promise((resolve,reject) => {
+        return new Promise((resolve, reject) => {
           const req = http.request('http://localhost:7001/info?id=4', (res) => {
             let data = '';
             res.on('data', (chunk) => {
@@ -55,9 +54,8 @@ describe('worker', () => {
 
   describe('list', () => {
     it('get list', async () => {
-     
       function getList() {
-        return new Promise((resolve,reject) => {
+        return new Promise((resolve, reject) => {
           const req = http.request('http://localhost:7001/list', (res) => {
             let data = '';
             res.on('data', (chunk) => {
@@ -80,37 +78,36 @@ describe('worker', () => {
     });
   });
 
-//   describe('remove',() => {
-//     it('menghapus data pekerja berdasarkan id', async () => {
-//       const options = {
-//         hostname: 'localhost',
-//         port: '7001',
-//         path: '/remove',
-//         method: 'DELETE',
-//       };
+  //   describe('remove',() => {
+  //     it('menghapus data pekerja berdasarkan id', async () => {
+  //       const options = {
+  //         hostname: 'localhost',
+  //         port: '7001',
+  //         path: '/remove',
+  //         method: 'DELETE',
+  //       };
 
-//       function removeWorker() {
-//         return new Promise((resolve,reject) => {
-//           const req = http.request(options, (res) => {
-//             let data = '';
-//             res.on('data', (chunk) => {
-//               data += chunk.toString();
-//             });
-//             res.on('end', () => {
-//               const removeData = JSON.parse(data);
-//               resolve(removeData);
-//             });
-//             res.on('error', (err) => {
-//               reject(err);
-//             });
-//           });
-//           req.end();
-//         });
-//       }
+  //       function removeWorker() {
+  //         return new Promise((resolve,reject) => {
+  //           const req = http.request(options, (res) => {
+  //             let data = '';
+  //             res.on('data', (chunk) => {
+  //               data += chunk.toString();
+  //             });
+  //             res.on('end', () => {
+  //               const removeData = JSON.parse(data);
+  //               resolve(removeData);
+  //             });
+  //             res.on('error', (err) => {
+  //               reject(err);
+  //             });
+  //           });
+  //           req.end();
+  //         });
+  //       }
 
-//       const hasil = await removeWorker();
-//       expect(hasil.name).toBe('ana');
-//     });
-//   });
+  //       const hasil = await removeWorker();
+  //       expect(hasil.name).toBe('ana');
+  //     });
+  //   });
 });
-
